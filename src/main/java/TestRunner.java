9196242373ee999028de6128c0203ccfcd75e3fd
Chetrusca Victoria <chetruscav6@gmail.com>
2023-03-27 20:25:15 +0300

    Creating a web driver manager and accesing a web page

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,5 +1,12 @@
+import managers.WebDriverManager;
+
 public class TestRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager webDriverManager=new WebDriverManager("CHROME");
+        webDriverManager.getDriver().get("https://www.w3schools.com/");
+
+        Thread.sleep(10000);
+        webDriverManager.closeDriver();
 
     }
 }
